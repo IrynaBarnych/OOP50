1e93db7705a53e4e0bf2df5e8a6ffe7713cb1423
IrynaBarnych <iryna.barnych@gmail.com>
2024-02-07 20:53:11 +0200

    Напишіть програму, яка створює список цілих чисел та виводить новий список, який містить лише парні числа з вихідного списку.

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# 5. Напишіть функцію, яка приймає список рядків від
-# користувача і повертає новий список, що містить лише
-# рядки, що починаються з великої літери.
+# 6. Напишіть функцію, яка приймає список рядків від
+# користувача і повертає новий список, що містить лише рядки, які містять слово "Python".
 
-def function_user(words):
-    return [word for word in words if word[0].isupper()]
+def line_python(lines):
+    return [line for line in lines if "Python" in line]
 
-user_words = []
+user_lines = []
 n = int(input("Введіть кількість рядків: "))
 for i in range(n):
-    user_string = input("Введіть рядок {}: ".format(i + 1))
-    user_words.append(user_string)
+    user_line = input("Введіть рядок {}: ".format(i + 1))
+    user_lines.append(user_line)
 
-list = function_user(user_words)
+filtered_lines = line_python(user_lines)
 
-print("новий список, що містить лише рядки, що починаються з великої літери:")
-for word in list:
-    print(word)
+print("Новий список, що містить лише рядки, що містять слово 'Python':")
+for line in filtered_lines:
+    print(line)
+
 
 
 
