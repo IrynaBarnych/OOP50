7de66620e6d3f6ce11bd5b789fbcd07f4cb25910
IrynaBarnych <iryna.barnych@gmail.com>
2024-02-07 20:26:47 +0200

    Напишіть програму, для знаходження суми всіх парних чисел від 1 до 100

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-# Напишіть програму, яка приймає два цілих числа від
-# користувача і виводить суму діапазону чисел між ними.
-
-# варіант 1
-
-def sum_range(num1, num2):
-    total_sum = num1 + num2
-    return total_sum
-
-num1 = int(input("Введіть перше ціле число: "))
-num2 = int(input("Введіть друге ціле число: "))
-
-print(f"Сума діапазону чисел між {num1} і {num2} дорівнює: {sum_range(num1, num2)}")
-
-# варіант 2
-def sum_diap(num1, num2):
-    start = min(num1, num2)
-    end = max(num1, num2)
-
-    total_sum = 0
-
-    for num in range(start + 1, end):
-        total_sum += num
-
-    return total_sum
+# 2. Напишіть програму, для знаходження суми всіх парних чисел від 1 до 100.
 
 
-num1 = int(input("Введіть перше ціле число: "))
-num2 = int(input("Введіть друге ціле число: "))
+total_sum = 0
 
-print(f"Сума діапазону чисел між {num1} і {num2} дорівнює: {sum_diap(num1, num2)}")
 
+for number in range(1, 101):
+    if number % 2 == 0:
+        total_sum += number
+
+print("Сума всіх парних чисел від 1 до 100:", total_sum)
 
 
 
